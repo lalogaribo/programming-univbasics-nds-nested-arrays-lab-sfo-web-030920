60e524b1a006faac124d34365a90e43af6d6b596
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-12 15:43:29 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,8 +39,8 @@ sorted_array
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+  array = matrix[row][column]
+  array
 end
 
 def matrix_update(matrix, row, column, new_value)
